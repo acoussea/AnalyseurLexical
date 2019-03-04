2b33947ffbbd2229339a3879c60a1a2610381712
acoussea <cousseau.axel@gmail.com>
2019-03-04 21:04:20 +0100

    Ajout d'un menu textuel 'Moteur' permettant d'effectuer tous les tests pour la présentation

--- src/analyseurlexical/Moteur.java
+++ src/analyseurlexical/Moteur.java
@@ -73,25 +73,70 @@ public class Moteur {
         //Automate NDtoD = afnd.dertiminiser(al.getAutomate());
         
         //afnd.descrToDot(NDtoD, "NDD01");
-        boolean scan=true,menu1=true,menu2=true,menu3=true;
+        
+        boolean menu1=true,menu2=true,menu3=true;
         while(menu1){
             System.out.println("----------SELECTION----------");
             System.out.println("1 - Analyse automate non-deterministe");
             System.out.println("2 - Analyse automate deterministe");
+            System.out.println("3 - Quitter ");
             Scanner sc1 = new Scanner(System.in);
             String s = sc1.nextLine();
             switch(s){
                 case "1" :
+                    System.out.println("Automate non-determinisé :");
                     AnalyseurLexical al1 = new AnalyseurLexical();
+                    al1.afficheDescrLignes();
                     AEFND nonDet = new AEFND();
-                    nonDet.dertiminiser(al1.getAutomate());
+                    nonDet.genereDescr(nonDet.dertiminiser(al1.getAutomate()), al1.getDescr().getName());
+                    System.out.println("Automate determinisé : ");
+                    AnalyseurLexical aldet = new AnalyseurLexical(new File(al1.getDescr().getName()+".descr"));
+                    aldet.afficheDescrLignes();
+                    while(menu2){
+                        System.out.println("-----Analyse Automate non-Deterministe-----");
+                        System.out.println("1 - Analyse d'entrée ");
+                        System.out.println("2 - Exporter l'automate determinisé en .dot ");
+                        System.out.println("3 - Retour ");
+                        Scanner sc3 = new Scanner(System.in);
+                        String s3 = sc3.nextLine();
+                        switch(s3){
+                            case "1" :
+                                boolean saisie=true;
+                                System.out.println("Entrez les mots à analyser, séparés par un retour à la ligne");
+                                System.out.println("Pour signifier la fin de la saisie, entrez ###");
+                                Scanner scEntree2 = new Scanner(System.in);
+                                String strEntree="";
+                                while(saisie){
+                                    System.out.print(">");
+                                    String str2 = scEntree2.nextLine();
+                                    strEntree += str2+'\n';
+                                    if(str2.equals("###")){
+                                        saisie=false;
+                                    }
+                                }
+                                aldet.traitementEntree(strEntree);
+                                break;
+                            case "2" :
+                                System.out.println("Export du fichier .descr en fichier .dot");
+                                aldet.descrToDot();
+                                break;
+                            case "3" :
+                                menu2=false;
+                                break;
+                            default :
+                                System.out.println("Invalide, tapez 1, 2 ou 3 pour signifier votre choix");
+                                break;
+                        }
+                    }
                     break;
                 case "2" :
                     AnalyseurLexical al2 = new AnalyseurLexical();
+                    al2.afficheDescrLignes();
                     while(menu3){
                         System.out.println("-----Analyse Automate Deterministe-----");
                         System.out.println("1 - Analyse d'entrée ");
                         System.out.println("2 - Exporter l'automate en .dot ");
+                        System.out.println("3 - Retour ");
                         Scanner sc2 = new Scanner(System.in);
                         String s2 = sc2.nextLine();
                         switch(s2){
@@ -99,30 +144,36 @@ public class Moteur {
                                 boolean saisie=true;
                                 System.out.println("Entrez les mots à analyser, séparés par un retour à la ligne");
                                 System.out.println("Pour signifier la fin de la saisie, entrez ###");
-                                Scanner sc = new Scanner(System.in);
-                                String str="";
+                                Scanner scEntree2 = new Scanner(System.in);
+                                String strEntree="";
                                 while(saisie){
                                     System.out.print(">");
-                                    String str2 = sc.nextLine();
-                                    str += str2+'\n';
+                                    String str2 = scEntree2.nextLine();
+                                    strEntree += str2+'\n';
                                     if(str2.equals("###")){
                                         saisie=false;
                                     }
                                 }
-                                al2.traitementEntree(str);
+                                al2.traitementEntree(strEntree);
                                 break;
                             case "2" :
                                 System.out.println("Export du fichier .descr en fichier .dot");
                                 al2.descrToDot();
                                 break;
+                            case "3" : 
+                                menu3=false;
+                                break;
                             default :
-                                System.out.println("Invalide, tapez 1 ou 2 pour signifier votre choix");
+                                System.out.println("Invalide, tapez 1, 2 ou 3 pour signifier votre choix");
                                 break;
                         }
                     }
                     break;
+                case "3" :
+                    menu1=false;
+                    break;
                 default :
-                    System.out.println("Invalide, tapez 1 ou 2 pour signifier votre choix");
+                    System.out.println("Invalide, tapez 1, 2 ou 3 pour signifier votre choix");
                     break;
             }
         }
